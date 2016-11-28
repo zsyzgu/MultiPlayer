71f6af9b4bfaed757cef2b19066f372e61e9eece
zsyzgu <zsyzgu@163.com>
2016-11-28 22:23:12 +0800

    Tank is running.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Spawner : NetworkBehaviour {
+    public GameObject tankPrefab;
+    public int numberOfTanks;
+
+    public override void OnStartServer() {
+        for (int i = 0; i < numberOfTanks; i++) {
+            Vector3 spawnPosition = new Vector3(Random.Range(-10.0f, 10.0f), 0.0f, Random.Range(-10.0f, 10.0f));
+            Quaternion spawnRotation = Quaternion.Euler(0.0f, Random.Range(0.0f, 180.0f), 0.0f);
+
+            GameObject tank = (GameObject)Instantiate(tankPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(tank);
+        }
+    }
+}
---
+++ Assets/Scripts/TankControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TankControl : MonoBehaviour {
+	void Start () {
+	
+	}
+	
+	void Update () {
+        transform.Translate(0, 0, Time.deltaTime);
+	}
+}
